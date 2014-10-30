60130ff41ee951412b73f0deadb7e1a361386bce
Patrick Eigensatz <patrick.eigensatz@gmail.com>
2014-10-30 01:57:09 +0100

    Einfaches Vollbildschirmfenster

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <SFML/Window.hpp>
 
 int main(void)
 {
+    sf::Window fenster(sf::VideoMode(1024, 768), "Robber", sf::Style::Fullscreen);
+
+    // Solange das Fenster geöffnet ist
+    while(fenster.isOpen())
+    {
+        sf::Event event;
+        fenster.pollEvent(event);
+
+        if(event.type == sf::Event::Closed){
+            fenster.close();
+        }
+    }
+
+    // Input Loop
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)){
+        fenster.close();
+    }
+
+
     return 0;
 }
