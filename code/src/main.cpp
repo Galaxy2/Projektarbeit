a4c5b8da81a86645141e86ba872b8d47756706d9
Patrick Eigensatz <patrick.eigensatz@gmail.com>
2014-10-30 13:50:37 +0100

    Fenster startet in voller Auflösung, Fokus auf Fenster

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -3,24 +3,30 @@
 
 int main(void)
 {
-    sf::Window fenster(sf::VideoMode(1024, 768), "Robber", sf::Style::Fullscreen);
+    sf::Window fenster(sf::VideoMode::getDesktopMode(), "Robber", sf::Style::Fullscreen);
 
     // Solange das Fenster geöffnet ist
     while(fenster.isOpen())
     {
         sf::Event event;
-        fenster.pollEvent(event);
+        while(fenster.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed){
+                fenster.close();
+            }
 
-        if(event.type == sf::Event::Closed){
-            fenster.close();
+
+            // Input Loop
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)){
+                fenster.close();
+            }
+
+            fenster.setActive();
+            fenster.display();
+
         }
-    }
 
-    // Input Loop
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)){
-        fenster.close();
     }
-
 
     return 0;
 }
