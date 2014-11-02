ac7ff3e54b91b0cb4816b54757e974d7f6809650
Jan Kunzmann <juppiey@gmail.com>
2014-11-02 13:00:25 +0100

    Problem mit aufloesung, suche Lösung

--- code/src/screen.cpp
+++ code/src/screen.cpp
@@ -11,6 +11,10 @@ void hintergrundLaden(sf::Sprite& s, sf::Texture& h){
 
     // Berechne Skalierungsfaktor
     float factor = aufloesung.width/1920.0f;
+    std:: cout << factor;
+    if (factor > 1){
+            factor = 1;
+    }
 
     s.setTexture(h);
     s.setScale(factor, factor);
