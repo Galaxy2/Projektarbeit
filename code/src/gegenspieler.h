a7e13900e95a56bed4a4bf9714d949ac1429fe58
Patrick Eigensatz <patrick.eigensatz@gmail.com>
2015-02-06 12:49:25 +0100

    no idea

---
+++ code/src/gegenspieler.h
@@ -0,0 +1,13 @@
+#ifndef __GEGENSPIELER_H__
+#define __GEGENSPIELER_H__
+
+#include <vector>
+#include <SFML/Graphics.hpp>
+
+class gegenspieler
+{
+    public:
+    vector<sf::waypoints
+};
+
+#endif // __GEGENSPIELER_H__
